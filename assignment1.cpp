179b7b67732df352960ef95ac8b186222ee114ad
vasudhapeesari <126183994+vasudhapeesari@users.noreply.github.com>
2023-02-24 19:52:54 +0530

    Day1 assignment submitted

---
+++ assignment1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cout<<"enter the first integer" <<endl;
+	cin>>a;
+	cout<<"enter the second integer" <<endl;
+	cin>>b;
+	if(a>b){
+		cout<<"first integer is greater" <<endl;
+	}
+	else{
+		cout<<"second integer is greater" <<endl;
+	}
+
+	return 0;
+}
